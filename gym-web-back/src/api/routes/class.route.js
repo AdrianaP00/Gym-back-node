d1e8d9e345770904bead0b96637d41f237a77b7a
Adriana Piccolo <adrianapiccolo00@gmail.com>
2023-08-23 18:52:58 +0200

    add swagger

--- gym-web-back/src/api/routes/class.route.js
+++ gym-web-back/src/api/routes/class.route.js
@@ -4,9 +4,61 @@ const classRouter = express.Router();
 const {isAuth, isCoach} = require("../../middlewares/auth")
 
 const {getClass, getOneClass, getOneClassByName, getOneClassByType,putClass,postClass ,deleteClass} = require("../controllers/class.controller")
+/**
+ * @swagger
+ * tags:
+ *   name: Classes
+ *   description: Endpoints to manage classes
+ */
 
+/**
+ * @swagger
+ * /class:
+ *   get:
+ *     summary: get all classes
+ *     responses:
+ *       200:
+ *         description: all classes
+ */
 classRouter.get("/",[isAuth], getClass);
+
+/**
+ * @swagger
+ * /class/{id}:
+ *   get:
+ *     summary: Obtain information about a specific class by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Class ID
+ *     responses:
+ *       200:
+ *         description: Obtain information about a specific class by ID
+ *       404:
+ *         description: Class not found
+ */
 classRouter.get("/:id",[isAuth], getOneClass)
+/**
+ * @swagger
+ * /class/{name}:
+ *   get:
+ *     summary: Obtain information about a specific class by name
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: name
+ *     responses:
+ *       200:
+ *         description: Obtain information about a specific class by name
+ *       404:
+ *         description: Class not found
+ */
 classRouter.get("/findByName/:name",[isAuth], getOneClassByName)
 classRouter.get("/findByType/:Type",[isAuth], getOneClassByType)
 classRouter.put("/modifyClass/:id",[isCoach], putClass)
